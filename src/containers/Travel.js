4070e77729b41bfde22108cf907d82f0792eab59
Becks Simpson <becks.simpson@outlook.com>
2019-11-16 15:07:00 +0000

    Travel information at bottom of screen

--- src/containers/Travel.js
+++ src/containers/Travel.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import '../App.scss';
 import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBiking, faTrain } from '@fortawesome/free-solid-svg-icons'
 
 class Travel extends Component {
   constructor(props){
     super(props)
     this.state={
-
+      bike: {occupancy: 8, total: 20},
+      train: [{name: "SMI", destination:"Matinkyla - Vusaari" , when:8 }, {name: "F2" , destination:"Matinkyla - Vusaari", when:4 }]
     }
   }
 
@@ -18,11 +20,28 @@ class Travel extends Component {
   render(){
     return(
       <div class ="travel-container">
-        Travel
+        <div class="bike">
+          <span><FontAwesomeIcon icon={faBiking}/></span> 
+          <span><h3>{this.state.bike.occupancy} / {this.state.bike.total}</h3></span>
+        </div>
+        {this.state.train.map(t=>
+          <div class="train">
+            <span><FontAwesomeIcon icon={faTrain}/></span> 
+            <span>
+              <h3>
+                <b>{t.name}</b> {t.destination} {t.when} mins
+              </h3>
+            </span>
+
+          </div> 
+        )}
+
       </div>
     )
   }
 
 }
+
+// <p>{t.when.slice(1,Math.min(3, t.when.length) ).join(', ')}...</p>
 
 export default Travel;
